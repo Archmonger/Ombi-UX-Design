db4319f680f4aad5fc272e0731137ccc027fb920
Archmonger <16909269+Archmonger@users.noreply.github.com>
2019-02-13 15:23:15 -0800

    aquaman modal preview complete!

--- js/script.js
+++ js/script.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 function showAquamanModal() {
-	$(".modal-tint.aquamman").css("color", "#FFF");
-}
-
-function hideAquamanModal() {
-	$(".modal-tint.aquamman").css("color", "#FFF");
-=======
-function showModal() {
-	$(".modal-tint").css("display", "block");
+	$(".aquaman.modal-tint").css("display", "block");
 }
 
 function hideModal() {
 	$(".modal-tint").css("display", "none");
->>>>>>> e01c30faacb02e05b916ab49e6ced90c0e769461
 }
