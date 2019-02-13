7e2b8dcf6a0982e3bbf2808d7b1857e1824eb7b4
Archmonger <16909269+Archmonger@users.noreply.github.com>
2019-02-13 15:42:24 -0800

    finish TV modal

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ function showAquamanModal() {
 	$(".aquaman.modal-tint").css("display", "block");
 }
 
+function showFlashModal() {
+	$(".flash.modal-tint").css("display", "block");
+}
+
 function hideModal() {
 	$(".modal-tint").css("display", "none");
 }
