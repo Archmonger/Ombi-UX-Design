5fdf66e6809bcef9c9ee93e2af7a5bf25c1952cd
Archmonger <16909269+Archmonger@users.noreply.github.com>
2019-02-13 15:51:27 -0800

    modal request btn javascript

--- js/script.js
+++ js/script.js
@@ -6,6 +6,18 @@ function showFlashModal() {
 	$(".flash.modal-tint").css("display", "block");
 }
 
+function flashModalRequestedBtn() {
+	$(".modal-request-button.flash i").css("display", "none");
+	$(".modal-request-button.flash").text("REQUESTED");
+	$(".modal-request-button.flash").css("background", "#cc7b19");
+}
+
+function aquamanModalRequestedBtn() {
+	$(".modal-request-button.aquaman i").css("display", "none");
+	$(".modal-request-button.aquaman").text("REQUESTED");
+	$(".modal-request-button.aquaman").css("background", "#cc7b19");
+}
+
 function hideModal() {
 	$(".modal-tint").css("display", "none");
 }
