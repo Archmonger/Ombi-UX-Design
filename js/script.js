dfd258d43423eb3d9c08ddfc0d4049d56939b63c
Archmonger <>
2019-02-25 01:37:48 -0800

    sidenav and modal animations

--- js/script.js
+++ js/script.js
@@ -1,3 +1,87 @@
+var hide_discover = 0;
+
+function hideDiscover() {
+	if (hide_discover == 0) {
+		$(".navpages-page.group.discover").css("border-bottom-color", "rgba(0,0,0,0)");
+		$(".hidden.discover").css("max-height", "0px");
+		hide_discover = 1;
+		document.getElementById("discover-fa").classList.remove('fa-angle-down');
+		document.getElementById("discover-fa").classList.add('fa-angle-up');
+	} else {
+		$(".navpages-page.group.discover").css("border-bottom-color", "rgba(0, 0, 0, 0.1)");
+		$(".hidden.discover").css("max-height", "1000px");
+		hide_discover = 0;
+		document.getElementById("discover-fa").classList.remove('fa-angle-up');
+		document.getElementById("discover-fa").classList.add('fa-angle-down');
+	}
+}
+
+var hide_user = 0;
+
+function hideUser() {
+	if (hide_user == 0) {
+		$(".navpages-page.group.user").css("border-bottom-color", "rgba(0,0,0,0)");
+		$(".hidden.user").css("max-height", "0px");
+		hide_user = 1;
+		document.getElementById("user-fa").classList.remove('fa-angle-down');
+		document.getElementById("user-fa").classList.add('fa-angle-up');
+	} else {
+		$(".navpages-page.group.user").css("border-bottom-color", "rgba(0, 0, 0, 0.1)");
+		$(".hidden.user").css("max-height", "1000px");
+		hide_user = 0;
+		document.getElementById("user-fa").classList.remove('fa-angle-up');
+		document.getElementById("user-fa").classList.add('fa-angle-down');
+	}
+}
+
+var hide_admin = 0;
+
+function hideAdmin() {
+	if (hide_admin == 0) {
+		$(".hidden.admin").css("max-height", "0px");
+		hide_admin = 1;
+		document.getElementById("admin-fa").classList.remove('fa-angle-down');
+		document.getElementById("admin-fa").classList.add('fa-angle-up');
+	} else {
+		$(".hidden.admin").css("max-height", "1000px");
+		hide_admin = 0;
+		document.getElementById("admin-fa").classList.remove('fa-angle-up');
+		document.getElementById("admin-fa").classList.add('fa-angle-down');
+	}
+}
+
+var hide_season_one = 1;
+
+function hideSeasonOne() {
+	if (hide_season_one == 0) {
+		$(".episode-container.season-one").css("max-height", "0px");
+		hide_season_one = 1;
+		document.getElementById("season-one-fa").classList.remove('fa-angle-up');
+		document.getElementById("season-one-fa").classList.add('fa-angle-down');
+	} else {
+		$(".episode-container.season-one").css("max-height", "1000px");
+		hide_season_one = 0;
+		document.getElementById("season-one-fa").classList.remove('fa-angle-down');
+		document.getElementById("season-one-fa").classList.add('fa-angle-up');
+	}
+}
+
+var hide_season_two = 1;
+
+function hideSeasonTwo() {
+	if (hide_season_two == 0) {
+		$(".episode-container.season-two").css("max-height", "0px");
+		hide_season_two = 1;
+		document.getElementById("season-two-fa").classList.remove('fa-angle-up');
+		document.getElementById("season-two-fa").classList.add('fa-angle-down');
+	} else {
+		$(".episode-container.season-two").css("max-height", "1000px");
+		hide_season_two = 0;
+		document.getElementById("season-two-fa").classList.remove('fa-angle-down');
+		document.getElementById("season-two-fa").classList.add('fa-angle-up');
+	}
+}
+
 function showAquamanModal() {
 	$(".aquaman.modal-tint").css("display", "block");
 }
